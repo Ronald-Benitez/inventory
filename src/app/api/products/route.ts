0a0d0e6dba62c795725b63665b4bac8a9a56e933
Empanada <58644435+Ronald-Benitez@users.noreply.github.com>
2023-12-12 16:58:58 -0600

    Filters and order by in inventory table

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,1 +1,20 @@
- 
+import { PrismaClient } from "@prisma/client";
+import { NextResponse, NextRequest } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function POST(req: NextRequest) {
+  try {
+    const data = await req.json();
+    const result = await prisma.products.create({
+      data,
+    });
+    return NextResponse.json(result, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: "Error al registrar la empresa" },
+      { status: 500 }
+    );
+  }
+}
